c1fe0cca8c9279656b4a846c39b3cb3c889b01e9
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 20:39:43 -0300

    test: currency and common constructor

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,25 +1,28 @@
 import { Money } from './money'
 
 describe('Money', () => {
-  describe('Dollar', () => {
-    it('Should correctly handle dollar multiplication', () => {
-      const five: Money = Money.dollar(5)
-      expect(five.times(2).toEquals(Money.dollar(10))).toBeTruthy()
-      expect(five.times(3).toEquals(Money.dollar(15))).toBeTruthy()
-    })
-    it('Should correctly handle equality', () => {
-      expect(Money.dollar(10).toEquals(Money.dollar(10))).toBeTruthy()
-      expect(Money.dollar(5).toEquals(Money.dollar(10))).toBeFalsy()
-      expect(Money.euro(10).toEquals(Money.euro(10))).toBeTruthy()
-      expect(Money.euro(5).toEquals(Money.euro(10))).toBeFalsy()
-      expect(Money.euro(10).toEquals(Money.dollar(10))).toBeFalsy()
-    })
+  it('Should correctly handle dollar multiplication', () => {
+    const five: Money = Money.dollar(5)
+    expect(five.times(2).toEquals(Money.dollar(10))).toBeTruthy()
+    expect(five.times(3).toEquals(Money.dollar(15))).toBeTruthy()
   })
-  describe('Euro', () => {
-    it('Should correctly handle euro multiplication', () => {
-      const five: Money = Money.euro(5)
-      expect(five.times(2).toEquals(Money.euro(10))).toBeTruthy()
-      expect(five.times(3).toEquals(Money.euro(15))).toBeTruthy()
-    })
+
+  it('Should correctly handle equality', () => {
+    expect(Money.dollar(10).toEquals(Money.dollar(10))).toBeTruthy()
+    expect(Money.dollar(5).toEquals(Money.dollar(10))).toBeFalsy()
+    expect(Money.euro(10).toEquals(Money.euro(10))).toBeTruthy()
+    expect(Money.euro(5).toEquals(Money.euro(10))).toBeFalsy()
+    expect(Money.euro(10).toEquals(Money.dollar(10))).toBeFalsy()
+  })
+
+  it('Should correctly handle euro multiplication', () => {
+    const five: Money = Money.euro(5)
+    expect(five.times(2).toEquals(Money.euro(10))).toBeTruthy()
+    expect(five.times(3).toEquals(Money.euro(15))).toBeTruthy()
+  })
+
+  it('Should correctly handle currencies', () => {
+    expect(Money.dollar(1).currency()).toEqual('USD')
+    expect(Money.euro(1).currency()).toEqual('EUR')
   })
 })
