932964c9876698b7b0eba7718c6e18cef200768e
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 15:14:55 -0300

    feat: base euro class

---
+++ src/euro.ts
@@ -0,0 +1,15 @@
+
+export class Euro {
+  private readonly amount: number
+  constructor (amount: number) {
+    this.amount = amount
+  }
+
+  times (multiplier: number): Euro {
+    return new Euro(this.amount * multiplier)
+  }
+
+  toEquals (other: Euro): boolean {
+    return this.amount === other.amount
+  }
+}
