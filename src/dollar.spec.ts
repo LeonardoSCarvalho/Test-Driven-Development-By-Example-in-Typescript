13f08128d9fe0db8e9044db47931560c406ebc58
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 14:47:49 -0300

    test: make amount private

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -3,12 +3,8 @@ import { Dollar } from './dollar'
 describe('Dollar', () => {
   it('Should return 10 when five dollars are multiplied by two', () => {
     const five: Dollar = new Dollar(5)
-
-    let product: Dollar = five.times(2)
-    expect(product.amount).toEqual(10)
-
-    product = five.times(3)
-    expect(product.amount).toEqual(15)
+    expect(five.times(2).toEquals(new Dollar(10))).toBeTruthy()
+    expect(five.times(3).toEquals(new Dollar(15))).toBeTruthy()
   })
   it('Should return true when dollars of the same value are compared', () => {
     expect(new Dollar(10).toEquals(new Dollar(10))).toBeTruthy()
