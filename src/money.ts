6e9dca3300fa4c17a00a54bc0c88e818c3aa546e
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 16:23:40 -0300

    feat: compare euros with dollars

--- src/money.ts
+++ src/money.ts
@@ -2,6 +2,7 @@ export class Money {
   public amount: number
 
   toEquals (other: Money): boolean {
+    if (this.constructor !== other.constructor) return false
     return this.amount === other.amount
   }
 }
