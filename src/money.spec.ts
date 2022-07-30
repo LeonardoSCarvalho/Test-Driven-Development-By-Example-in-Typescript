8fa0018990fa476b05e56f0ff3c244b5d83879d8
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 17:14:22 -0300

    test: create factory methods for dollars and euros

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,26 +1,25 @@
-import { Dollar } from './dollar'
-import { Euro } from './euro'
+import { Money } from './money'
 
 describe('Money', () => {
   describe('Dollar', () => {
     it('Should correctly handle dollar multiplication', () => {
-      const five: Dollar = new Dollar(5)
-      expect(five.times(2).toEquals(new Dollar(10))).toBeTruthy()
-      expect(five.times(3).toEquals(new Dollar(15))).toBeTruthy()
+      const five: Money = Money.dollar(5)
+      expect(five.times(2).toEquals(Money.dollar(10))).toBeTruthy()
+      expect(five.times(3).toEquals(Money.dollar(15))).toBeTruthy()
     })
     it('Should correctly handle equality', () => {
-      expect(new Dollar(10).toEquals(new Dollar(10))).toBeTruthy()
-      expect(new Dollar(5).toEquals(new Dollar(10))).toBeFalsy()
-      expect(new Euro(10).toEquals(new Euro(10))).toBeTruthy()
-      expect(new Euro(5).toEquals(new Euro(10))).toBeFalsy()
-      expect(new Dollar(10).toEquals(new Euro(10))).toBeFalsy()
+      expect(Money.dollar(10).toEquals(Money.dollar(10))).toBeTruthy()
+      expect(Money.dollar(5).toEquals(Money.dollar(10))).toBeFalsy()
+      expect(Money.euro(10).toEquals(Money.euro(10))).toBeTruthy()
+      expect(Money.euro(5).toEquals(Money.euro(10))).toBeFalsy()
+      expect(Money.euro(10).toEquals(Money.dollar(10))).toBeFalsy()
     })
   })
   describe('Euro', () => {
     it('Should correctly handle euro multiplication', () => {
-      const five: Euro = new Euro(5)
-      expect(five.times(2).toEquals(new Euro(10))).toBeTruthy()
-      expect(five.times(3).toEquals(new Euro(15))).toBeTruthy()
+      const five: Money = Money.euro(5)
+      expect(five.times(2).toEquals(Money.euro(10))).toBeTruthy()
+      expect(five.times(3).toEquals(Money.euro(15))).toBeTruthy()
     })
   })
 })
