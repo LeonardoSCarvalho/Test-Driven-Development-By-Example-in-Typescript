7cf53126f4a36e1868280a66805042f9645dbeb8
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 14:02:51 -0300

    feat: remove dollar side-effects

--- src/dollar.ts
+++ src/dollar.ts
@@ -5,7 +5,7 @@ export class Dollar {
     this.amount = amount
   }
 
-  times (multiplier: number) {
-    this.amount *= multiplier
+  times (multiplier: number):Dollar {
+    return new Dollar(this.amount * multiplier)
   }
 }
