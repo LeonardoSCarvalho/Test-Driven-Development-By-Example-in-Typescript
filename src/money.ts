cf0fee4a3515d950487f9b8315f19d40f114e377
leonardo <leo.carvalholkp@gmail.com>
2022-07-30 17:14:06 -0300

    feat: create factory methods for dollars and euros

--- src/money.ts
+++ src/money.ts
@@ -1,8 +1,40 @@
-export class Money {
+export abstract class Money {
   public amount: number
+
+  static dollar (amount: number):Money {
+    return new Dollar(amount)
+  }
+
+  static euro (amount: number):Money {
+    return new Euro(amount)
+  }
 
   toEquals (other: Money): boolean {
     if (this.constructor !== other.constructor) return false
     return this.amount === other.amount
+  }
+
+  abstract times(multiplier:number): Money
+}
+
+export class Dollar extends Money {
+  constructor (amount: number) {
+    super()
+    super.amount = amount
+  }
+
+  times (multiplier: number):Dollar {
+    return new Dollar(this.amount * multiplier)
+  }
+}
+
+export class Euro extends Money {
+  constructor (amount: number) {
+    super()
+    super.amount = amount
+  }
+
+  times (multiplier: number): Euro {
+    return new Euro(this.amount * multiplier)
   }
 }
