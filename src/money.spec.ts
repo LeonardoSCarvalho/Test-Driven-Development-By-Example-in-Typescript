1c6a00eeef1b15901ec5be471d69769fcc43080b
leonardo <leo.carvalholkp@gmail.com>
2022-07-31 15:56:53 -0300

    refactor: improve tests descriptions

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,19 +1,19 @@
 import { Money } from './money'
 
 describe('Money', () => {
-  it('Should correctly handle dollar multiplication', () => {
+  it('Should handle dollar multiplication', () => {
     const five: Money = Money.dollar(5)
     expect(five.times(2).toEquals(Money.dollar(10))).toBeTruthy()
     expect(five.times(3).toEquals(Money.dollar(15))).toBeTruthy()
   })
 
-  it('Should correctly handle equality', () => {
+  it('Should handle equality', () => {
     expect(Money.dollar(10).toEquals(Money.dollar(10))).toBeTruthy()
     expect(Money.dollar(5).toEquals(Money.dollar(10))).toBeFalsy()
     expect(Money.euro(10).toEquals(Money.dollar(10))).toBeFalsy()
   })
 
-  it('Should correctly handle currencies', () => {
+  it('Should handle currencies', () => {
     expect(Money.dollar(1).currency()).toEqual('USD')
     expect(Money.euro(1).currency()).toEqual('EUR')
   })
