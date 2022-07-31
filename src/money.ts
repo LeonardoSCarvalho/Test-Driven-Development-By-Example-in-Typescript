53dfe044994c70b4db535f640e860f2c17359453
leonardo <leo.carvalholkp@gmail.com>
2022-07-31 16:17:13 -0300

    feat: handle simple addition

--- src/money.ts
+++ src/money.ts
@@ -1,4 +1,6 @@
-export class Money {
+import { Expression } from './expression'
+
+export class Money implements Expression {
   private readonly amount: number
   private readonly _currency: string
   constructor (amount: number, currency: string) {
@@ -25,5 +27,9 @@ export class Money {
 
   times (multiplier: number):Money {
     return new Money(this.amount * multiplier, this._currency)
+  }
+
+  plus (addend: Money): Expression {
+    return new Money(this.amount + addend.amount, this._currency)
   }
 }
